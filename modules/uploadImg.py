f10852757f7d03259fdbeb6583af43691341a003
Santiago <igosantiago91@gmail.com>
2023-05-07 20:05:40 +0700

    service account path

--- modules/uploadImg.py
+++ modules/uploadImg.py
@@ -1,5 +1,5 @@
 from google.cloud import storage
 
 client = storage.Client.from_service_account_json(
-    'deploy-flask-testing-89a87c693e13.json')
+    'credentials.json')
 bucket = client.get_bucket('user-images-bucket-flask')
